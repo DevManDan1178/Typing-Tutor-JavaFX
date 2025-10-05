eb4d959bca60c1f4bd4cc65e954dd9631019d3ab
DevManDan1178 <l3r0d4niel@gmail.com>
2025-10-05 13:48:27 -0400

    + Interactive visual keyboard display (keys turn red when pressed, reset when let go)

--- Assignment_01/src/MainApp.java
+++ Assignment_01/src/MainApp.java
@@ -1,17 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
-
 /**
  *
- * @author nagatd
+ * @author Daniel Orejuela Liu
  */
+import java.util.HashMap;
 import javafx.application.Application;
+import javafx.event.EventType;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
 
 
 public class MainApp extends Application{
@@ -23,18 +25,42 @@ public class MainApp extends Application{
        public static void main(String[] args) {
         launch(args);
     }
-
+     
+    
     @Override
     public void start(Stage primaryStage) {
         
         primaryStage.setTitle("Typing Tutor - JavaFX Assignment");
 
         Pane root = new Pane();
-
+        
+        GridPane keyboardGrid = new GridPane();
+        root.getChildren().add(keyboardGrid);
+        HashMap<String, Button> keyboardMap =  TypingController.DisplayOntoRootPane(keyboardGrid);
+        
         Scene scene = new Scene(root, 800, 600);
+        scene.setOnKeyPressed((KeyEvent event) -> {
+            KeyCode pressedKey = event.getCode();
+            String keyStr = pressedKey.toString();
+            Button keyboardButton = keyboardMap.get(keyStr);
+            if (keyboardButton == null) {
+                return;
+            }
+            keyboardButton.setStyle("-fx-background-color: #ff0000;");
+        });
+        scene.setOnKeyReleased((KeyEvent event) -> {
+            KeyCode pressedKey = event.getCode();
+            String keyStr = pressedKey.toString();
+            Button keyboardButton = keyboardMap.get(keyStr);
+            if (keyboardButton == null) {
+                return;
+            }
+            keyboardButton.setStyle("");
+        });
         primaryStage.setScene(scene);
         primaryStage.show();
-
+        
+        
        
     }
 }
